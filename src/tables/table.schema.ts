5f30412f5d4d1c3def6e3f36b129bf94ac0a64fa
Erdem Alpay <erdemalpay@gmail.com>
2021-01-03 17:19:59 +0300

    Fix unique id in table

--- src/tables/table.schema.ts
+++ src/tables/table.schema.ts
@@ -12,10 +12,11 @@ export class Table extends Document {
   @Prop({ required: true })
   seat: number;
 
-  @Prop({ required: true, unique: true })
+  @Prop({ required: true })
   order: number;
 }
 
 export const TableSchema = SchemaFactory.createForClass(Table);
 TableSchema.index({ userId: 1 });
 TableSchema.index({ userId: 1, id: 1 }, { unique: true });
+TableSchema.index({ userId: 1, order: 1 }, { unique: true });
